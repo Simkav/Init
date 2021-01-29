c7a97245b1d044ab0f2c88233691fa591de563f9
Evhenii Lapshinov <koenigstag@gmail.com>
2021-01-29 16:23:59 +0200

    Change path of shell script copy

--- createInitialDirectory.sh
+++ createInitialDirectory.sh
@@ -3,7 +3,7 @@ mkdir ~/.InitialProject
 cp -rpa ./InitialProject/* ~/.InitialProject/
 cp -rpa ./InitialProject/.gitignore ~/.InitialProject/
 chmod 777 ~/.InitialProject
-sudo cp InitProject /usr/bin/
-sudo chmod 777 /usr/bin/InitProject
+sudo cp InitProject /usr/local/bin/
+sudo chmod 777 /usr/local/bin/InitProject
 
 echo 'Script createInitialDirectory.sh is finished.'
